299e7092971ce01be9ec0995a4b2812b1cb10b70
imranhussbd <imranhussbd@gmail.com>
2022-08-01 02:25:33 -0400

    QA

--- src/lec01_java_basics/ADF.java
+++ src/lec01_java_basics/ADF.java
@@ -3,8 +3,8 @@ package lec01_java_basics;
 public class ADF {
 
 	public static void main(String[] args) {
-		Add adf1 = new Add();
-		adf1.power();
+		
+		
 		
 		System.out.println("\n-----------------------\n");
 		
@@ -12,7 +12,7 @@ public class ADF {
 		adf2.conInfo(38, 'F', false);
 		
 		
-		
+		//QA 
 	}
 
 }
